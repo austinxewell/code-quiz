b35296972ba4fde877f08128f375b376be58ee34
AustinxEwell <austin.ewell86@gmail.com>
2021-08-11 17:47:10 -0600

    start button to console.log

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var startButton = document.querySelector("#start")
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+console.log("testing")
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
